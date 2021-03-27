43f81a9f3c2f67b96ee36d2470cfb65a87cbf263
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-03-27 21:58:00 +0100

    feat: modelagen da estrutrura

---
+++ lista-linear-sequencial.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#define MAX 50
+
+typedef int TIPOCHAVE;
+
+typedef struct {
+  TIPOCHAVE chave;
+} REGISTRO;
+
+typedef struct {
+  REGISTRO A[MAX];
+  int nroElem;
+} LISTA;
+
+
+int main() {
+
+
+
+  return 0;
+}

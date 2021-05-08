b3c1c0f3ee642aceed92fdba52fcb37c571c316d
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-05-08 13:04:15 +0100

    feat: modelagen

---
+++ lista-ligada-dinamica.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <malloc.h>
+
+typedef int TIPOCHAVE;
+
+typedef enum
+{
+  false = 0,
+  true = 1
+} boolean;
+
+typedef struct
+{
+  TIPOCHAVE chave;
+} REGISTRO;
+
+typedef struct aux
+{
+  REGISTRO reg;
+  struct aux *prox;
+} ELEMENTO;
+
+typedef ELEMENTO *PONT;
+
+typedef struct
+{
+  PONT inicio;
+} LISTA;
+
+int main()
+{
+
+  return 0;
+}

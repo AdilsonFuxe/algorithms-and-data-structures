3c827acf1c8d09801ff0a2e287d13c3468ee8fe3
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-05-14 21:23:33 +0100

    feat: atribuir matriz

--- matriz-esparsa.c
+++ matriz-esparsa.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <malloc.h>
 
+typedef enum
+{
+  false = 0,
+  true = 1
+} boolean;
+
 typedef struct aux
 {
   float valor;
@@ -15,8 +21,62 @@ typedef struct
   int colunas;
 } MATRIZ;
 
+void inicializarMatriz(MATRIZ *m, int lin, int col);
+boolean atribuirMatriz(MATRIZ *m, int lin, int col, float val);
+
 int main()
 {
 
   return 0;
+}
+
+void inicializarMatriz(MATRIZ *m, int lin, int col)
+{
+  int i;
+  m->linhas = lin;
+  m->colunas = col;
+  m->A = (PONT *)malloc(lin * sizeof(PONT));
+  for (i = 0; i < lin; i++)
+    m->A[i] = NULL;
+}
+
+boolean atribuirMatriz(MATRIZ *m, int lin, int col, float val)
+{
+  if (lin < 0 || lin >= m->linhas || col < 0 || col >= m->colunas)
+    return false;
+  PONT ant = NULL;
+  PONT atual = m->A[lin];
+  while (atual != NULL && atual->coluna < col)
+  {
+    ant = atual;
+    atual = atual->prox;
+  }
+
+  if (atual != NULL && atual->coluna == col)
+  {
+    if (val == 0)
+    {
+      if (ant == NULL)
+        m->A[lin] = atual->prox;
+      else
+        ant->prox = atual->prox;
+      free(atual);
+    }
+    else
+
+      atual->valor = val;
+  }
+  else
+  {
+    PONT novo = (PONT)malloc(sizeof(NO));
+    novo->coluna = col;
+    novo->valor = val;
+    novo->prox = atual;
+
+    if (ant == NULL)
+      m->A[lin] = novo;
+    else
+      ant->prox = novo;
+  }
+  return true;
 }

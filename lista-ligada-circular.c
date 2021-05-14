db0a2c82422331851b622e47af3dd110a609928e
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-05-14 09:27:59 +0100

    feat: modelagem da estrutrua circular

--- lista-ligada-circular.c
+++ lista-ligada-circular.c
@@ -1,4 +1,31 @@
 #include <stdio.h>
+#include <malloc.h>
+
+typedef int TIPOCHAVE;
+
+typedef enum
+{
+  false = 0,
+  true = 1
+} boolean;
+
+typedef struct
+{
+  TIPOCHAVE chave;
+} REGISTRO;
+
+typedef struct tempRegistro
+{
+  REGISTRO reg;
+  struct tempRegistro *prox;
+} ELEMENTO;
+
+typedef ELEMENTO *PONT;
+
+typedef struct
+{
+  PONT cabeca;
+} LISTA;
 
 int main()
 {
